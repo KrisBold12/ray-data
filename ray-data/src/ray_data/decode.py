c8a30d451faa51b355f503091f658702dbef1c19
KrisBold12 <kristianboldini@gmail.com>
2026-08-31 17:05:38 +0200

    Quick fix on pipeline

--- ray-data/src/ray_data/decode.py
+++ ray-data/src/ray_data/decode.py
@@ -2,13 +2,14 @@ import time
 import numpy as np
 
 NUM_FRAMES = 8
+SECONDS_FOR_SIMULATION = 0.05
 
 
 def decode(batch: dict[str, np.ndarray]):
     n = len(batch["id"])
     frames = []
     for i in range(n):
-        time.sleep(0.05) # Simulate decoding
+        time.sleep(SECONDS_FOR_SIMULATION) # Simulate decoding
         frames.append(np.random.randint(0, 256, size=(NUM_FRAMES, 64, 64, 3), dtype=np.uint8))
     batch["frames"] = np.stack(frames)
     return batch
--- ray-data/src/ray_data/pipeline.py
+++ ray-data/src/ray_data/pipeline.py
@@ -2,11 +2,15 @@ import ray
 import time
 
 from ray_data.dataset import create_mock_dataset
-from ray_data.decode import decode
+from ray_data.decode import decode, SECONDS_FOR_SIMULATION
+
+
+DS_SIZE = 1000
+CORES_ON_MACHINE_CPU = 8
 
 
 def main():
-    ds = ray.data.from_items(create_mock_dataset(1000))
+    ds = ray.data.from_items(create_mock_dataset(DS_SIZE))
     print(f"Size: {ds.count()}\nSchema: {ds.schema()}\nSamples: {ds.take(2)}\n# blocks: {ds.num_blocks()}")
 
     ds = ds.map_batches(decode)
@@ -15,7 +19,7 @@ def main():
     mat = ds.materialize()
     end = time.perf_counter()
 
-    print(f"Execution time: {end - start} | Expected execution time: {1000 * 0.05 / 8} | Throughput: {1000 / (end - start)}")
+    print(f"Execution time: {end - start} | Expected execution time: {DS_SIZE * SECONDS_FOR_SIMULATION / CORES_ON_MACHINE_CPU} | Throughput: {DS_SIZE / (end - start)}")
     print(mat.stats())
 
 
